04dde8b01985db3bf803cc9a96cf82cdd6dbff8f
Langster2323 <langahkeem@gmail.com>
2019-09-24 10:47:45 -0500

    Increase users

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export function loadUser(){
   return(dispatch) => {
     return axios.get('https://jsonplaceholder.typicode.com/users').then((res) => {
-      dispatch(fetchUser(res.data.slice(0, 4)));
+      dispatch(fetchUser(res.data.slice(0, 8)));
     })
   }
 }
